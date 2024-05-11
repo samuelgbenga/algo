427dbd87e24872acc32cdcba8b9786f1c183dc52
samuelgbenga <samuelgbenga972@gmail.com>
2024-05-11 10:45:38 +0100

    just algo

--- ROOMALLOCATIONALGO/src/AllocatingHotelRooms.java
+++ ROOMALLOCATIONALGO/src/AllocatingHotelRooms.java
@@ -15,71 +15,53 @@ public class AllocatingHotelRooms {
             departureArray[counter]= customer[1];
             counter++;
         }
+        // allocate room by arrival time
 
-        // lets get index with min arrival date
-//        int minArrival = getMinIndex(arrivalArray);
-//
-//        roomNumber[minArrival] = 1;
-
-       roomNumbers = roomAllocationByArrival(arrivalArray);
+        roomNumbers = getAllocateRoomByArrival(arrivalArray);
 
 
 
-        //System.out.println(Arrays.toString(roomNumbers));
+        System.out.println(Arrays.toString(roomNumbers));
+
         return new int[customers.length];
     }
 
-    // method to return room allocation in ascending order
-    public static int[] roomAllocationByArrival(int[] arrivalArray){
 
-        // allocate room by arrival date in ascending numerical order
-        int[] arrivalArr = new int[arrivalArray.length];
-        int roomNumber = 0;
-        for (int i = arrivalArray.length; i >= 0 ; i--) {
-            roomNumber++;
-                int minIndex = getMinIndex(arrivalArray);
-                arrivalArr[minIndex] = roomNumber;
-            System.out.println(minIndex);
-                arrivalArray = removeIndex(minIndex, arrivalArray);
-
-        }
-        return arrivalArr;
-    }
 
     // method to return the minimum index of the array
-    public static int getMinIndex(int[] array) {
-        if (array == null || array.length == 0) {
-            throw new IllegalArgumentException("Array must not be empty or null");
-        }
+    public static int[] getAllocateRoomByArrival(int[] array) {
+        int[] arrRoom = new int[array.length];
+
 
         int minIndex = 0;
-        for (int i = 1; i < array.length; i++) {
-            if (array[i] < array[minIndex]) {
-                minIndex = i;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length ; j++) {
+                if (array[j] < array[minIndex]) {
+                    minIndex = j;
+                }
             }
+            arrRoom[minIndex] = i+1;
+            array[minIndex] = Integer.MAX_VALUE;
+
         }
-        return minIndex;
+        //System.out.println(Arrays.toString(arrRoom));
+        return arrRoom;
     }
 
-    // method to remove detected index
-    public static int[] removeIndex(int index, int[] array){
-        if (array == null || array.length == 0) {
-            throw new IllegalArgumentException("Array must not be empty or null");
-        }
-        // let's try it
-        System.arraycopy(array, index+1, array, index, array.length-index-1);
-        array[array.length-1]= 0;
-        return array;
+    // update allocated room by departure time
+    public static int[] updateAllocatedRoom(int[] roomByArrival, int[] arrivalArray, int[] departureArray){
+
+
+        return new int[roomByArrival.length];
     }
+
 
     public static void main(String[] args) {
-        int[][] customers = {{5,2},{6,4}, {4,4}};
-       // int[] singleArr = {3,4,5,6,3,7};
+        int[][] customers = {{5,2},{6,4}, {4,4}, {1,5}, {1,4}};
+        //int[] singleArr = {3,4,5,6,8,1};
 
         int[] arr = allocateRooms(customers);
 
-        //int[] testing = removeIndex(2,singleArr);
-
-        //System.out.println(Arrays.toString(testing));
+    // next to update the allocated room by the departure time......
     }
 }
