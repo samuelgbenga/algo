aa118b4752cf632f840246c4d38e84d7bc9be86a
samuelgbenga <samuelgbenga972@gmail.com>
2024-05-28 15:11:59 +0100

    completed an assignment

--- INTERNSHIPFORM/src/week_6/practice/Main.java
+++ INTERNSHIPFORM/src/week_6/practice/Main.java
@@ -1,4 +1,14 @@
 package week_6.practice;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        List<Integer> evenList = Arrays.asList(1,2,3,4,5,6,7);
+
+        evenList.stream().filter((i)-> i%2 == 0 ).forEach(System.out::println);
+    }
 }
