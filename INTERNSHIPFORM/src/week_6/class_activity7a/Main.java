cae266c0ce98dcd3855517b9d0005ef7f90c10b5
samuelgbenga <samuelgbenga972@gmail.com>
2024-05-30 21:00:55 +0100

    algo with unit test

--- INTERNSHIPFORM/src/week_6/class_activity7a/Main.java
+++ INTERNSHIPFORM/src/week_6/class_activity7a/Main.java
@@ -1,8 +1,17 @@
 package week_6.class_activity7a;
 
+
 public class Main {
-
-
+//
+//   public static void talk(){
+//        System.out.println("hello world");
+//    };
+//    public static void main(String[] args) {
+//
+//       // SaySomething sayAble = Main::talk;
+//
+//        sayAble.say("hello world");
+//    }
 }
 
 
@@ -10,7 +19,5 @@ public class Main {
 interface SaySomething{
 
     void say(String msg);
-    static void talk(){
 
-    };
 }
