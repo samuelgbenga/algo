5652d3f3850328f9be2d1768c2f0ac885c989ca2
samuelgbenga <samuelgbenga972@gmail.com>
2024-06-03 08:06:35 +0100

    new algo to solve

---
+++ INTERNSHIPFORM/src/week_7/practice1/algo7/LongestSubstringWithoutDuplication.java
@@ -0,0 +1,85 @@
+package week_7.practice1.algo7;
+
+//Candidate Instructions
+//        Write a function that takes in a string and returns its longest substring
+//        without duplicate characters.
+//
+//        You can assume that there will only be one longest substring without duplication.
+//
+//        Sample Input
+//        string = "clementisacap"
+//
+//        Sample Output
+//        "mentisac"
+//        As you can see from the input above, the longest string we can take from the word
+//        that does not contain any duplicate characters is mentisac
+//
+//
+
+//Test cases
+//import org.junit.Test;
+//import static org.junit.Assert.*;
+//
+// class ProgramTests {
+//    @Test
+//    public void TestCase1() {
+//        assertEquals("mentisac", Program.longestSubstringWithoutDuplication("clementisacap"));
+//    }
+//
+//    @Test
+//    public void TestCase2() {
+//        assertEquals("a", Program.longestSubstringWithoutDuplication("a"));
+//    }
+//
+//    @Test
+//    public void TestCase3() {
+//        assertEquals("abc", Program.longestSubstringWithoutDuplication("abc"));
+//    }
+//
+//    @Test
+//    public void TestCase4() {
+//        assertEquals("abc", Program.longestSubstringWithoutDuplication("abcb"));
+//    }
+//
+//    @Test
+//    public void TestCase5() {
+//        assertEquals("abcdef", Program.longestSubstringWithoutDuplication("abcdeabcdefc"));
+//    }
+//
+//    @Test
+//    public void TestCase6() {
+//        assertEquals("cdea", Program.longestSubstringWithoutDuplication("abccdeaabbcddef"));
+//    }
+//
+//    @Test
+//    public void TestCase7() {
+//        assertEquals("bac", Program.longestSubstringWithoutDuplication("abacacacaaabacaaaeaaafa"));
+//    }
+//
+//    @Test
+//    public void TestCase8() {
+//        assertEquals("dabcef", Program.longestSubstringWithoutDuplication("abcdabcef"));
+//    }
+//
+//    @Test
+//    public void TestCase9() {
+//        assertEquals("cbde", Program.longestSubstringWithoutDuplication("abcbde"));
+//    }
+//
+//    @Test
+//    public void TestCase10() {
+//        assertEquals("vsindecago", Program.longestSubstringWithoutDuplication("decadevsindecagonarelit"));
+//    }
+//}
+
+
+public class LongestSubstringWithoutDuplication {
+    public static void main(String[] args) {
+        // test your cases here
+    }
+
+    public static String longestSubstringWithoutDuplication(String str) {
+        // Do your magic here
+        return "";
+    }
+}
