d88e5149b19d63422b98c5a7adfbea6593b28c1a
samuelgbenga <samuelgbenga972@gmail.com>
2024-06-25 07:04:53 +0100

    new algo

---
+++ INTERNSHIPFORM/src/week_7/practice1/graph_algo/algo6/BinarySearch.java
@@ -0,0 +1,32 @@
+package week_7.practice1.graph_algo.algo6;
+
+public class BinarySearch {
+    public static int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {-1, 0, 3, 5, 9, 12};
+        int target1 = 9;
+        System.out.println(search(nums1, target1)); // Output: 4
+
+        int[] nums2 = {-1, 0, 3, 5, 9, 12};
+        int target2 = 2;
+        System.out.println(search(nums2, target2)); // Output: -1
+    }
+}
