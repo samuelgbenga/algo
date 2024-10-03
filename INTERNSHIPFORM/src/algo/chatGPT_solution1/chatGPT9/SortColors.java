628fce76a5874eef2b8fd9c0a264a9774a9f5924
samuelgbenga <samuelgbenga972@gmail.com>
2024-10-03 09:21:39 +0100

    new commit

--- INTERNSHIPFORM/src/algo/chatGPT_solution1/chatGPT9/SortColors.java
+++ INTERNSHIPFORM/src/algo/chatGPT_solution1/chatGPT9/SortColors.java
@@ -1,11 +1,21 @@
 package algo.chatGPT_solution1.chatGPT9;
 
+import java.util.*;
+
 public class SortColors {
     public static void sortColors(int[] nums) {
-        int low = 0, mid = 0, high = nums.length - 1;
 
-        // Traverse the array
+        // 1 init low
+        int low = 0;
+        // 2 middle high
+        int mid = 0;
+        // 3 init high
+        int high = nums.length - 1;
+
+        //4 loop throught the array using while from middle to high
         while (mid <= high) {
+
+            //4.1 if the num at the middle is zero swap  num at low to num at middle then increase both low and mid
             if (nums[mid] == 0) {
                 // Swap nums[low] and nums[mid]
                 int temp = nums[low];
@@ -13,9 +23,13 @@ public class SortColors {
                 nums[mid] = temp;
                 low++;
                 mid++;
-            } else if (nums[mid] == 1) {
+            }
+            // 4.2 else if num at mid is 1 just increase mid
+            else if (nums[mid] == 1) {
                 mid++;
-            } else {  // nums[mid] == 2
+            }
+            // 4.3 else if num at mid 2 is swap it with the num at high then only decrease high
+            else {  // nums[mid] == 2
                 // Swap nums[mid] and nums[high]
                 int temp = nums[mid];
                 nums[mid] = nums[high];
@@ -30,12 +44,12 @@ public class SortColors {
         int[] nums1 = {2, 0, 2, 1, 1, 0};
         sortColors(nums1);
         // Output: [0, 0, 1, 1, 2, 2]
-        System.out.println(java.util.Arrays.toString(nums1));
+        System.out.println(Arrays.toString(nums1));
 
         // Example 2
         int[] nums2 = {2, 0, 1};
         sortColors(nums2);
         // Output: [0, 1, 2]
-        System.out.println(java.util.Arrays.toString(nums2));
+        System.out.println(Arrays.toString(nums2));
     }
 }
